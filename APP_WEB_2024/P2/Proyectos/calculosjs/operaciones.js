429df708ac2b11daf7ba728d58561ff4f40a5803
Leslie208l <lesliesosa080305@gmail.com>
2024-10-18 10:01:22 -0600

    2DO COMMIT PROYECTOS

---
+++ APP_WEB_2024/P2/Proyectos/calculosjs/operaciones.js
@@ -0,0 +1,22 @@
+
+function operacion ()
+{
+    let n1=document.getElementById("n1").values;
+    let n2=document.getElementById("n2").values;
+    let tipo=document.getElementById("tipo").values;
+
+    if (isnumber(n1) )
+    switch (tipoope)
+    {
+        case "suma":ope=n1+n2;break;
+        case "resta":ope=n1-n2;break;
+        case "multiplicacion":ope=n1*n2;break;
+        case "division":ope=n1/n2;break;
+    }
+
+    resul=document.getElementById("resultado").innerHTML=<h2>(n1).5(tipoope).$(n2).=</h2>;
+    
+}
+
+let.resultado=document.getElementById("resultado");
+resul.innerhtml=`<h2>$(ope)</h2>`
